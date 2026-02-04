caafa7437850f6a2c9e8b325a5b1b41c2eaa60e0
jea022 <aguinagajuanes2003@gmail.com>
2026-02-04 03:54:01 -0500

    Correcion Errores de Launch

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/nombre-del-repo/', // Replace 'nombre-del-repo' with the name of your GitHub repository
       server: {
         port: 3000,
         host: '0.0.0.0',
